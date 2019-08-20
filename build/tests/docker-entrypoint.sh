4e4036382b8216067cc856330009f82b9f1660d8
Julien Neuhart <j.neuhart@thecodingmachine.com>
2019-08-20 10:45:56 +0200

    improving load time for lint and tests commands

--- build/tests/docker-entrypoint.sh
+++ build/tests/docker-entrypoint.sh
@@ -15,20 +15,3 @@ go run github.com/thecodingmachine/gotenberg/test/cmd/pm2
 
 # Run our tests.
 go test -race -cover ./...
-
-# Testing PM2 processes launch separatly for avoiding
-# spending to much time on each tests depending on
-# them.
-#go test github.com/thecodingmachine/gotenberg/internal/pkg/pm2 -run TestChromeStart
-#go test github.com/thecodingmachine/gotenberg/internal/pkg/pm2 -run TestUnoconvStart
-
-# Running others tests.
-#go test -race -cover -covermode=atomic github.com/thecodingmachine/gotenberg/internal/pkg/config
-#go test -race -cover -covermode=atomic github.com/thecodingmachine/gotenberg/internal/pkg/random
-#go test -race -cover -covermode=atomic github.com/thecodingmachine/gotenberg/internal/pkg/standarderror
-#go test -race -cover -covermode=atomic github.com/thecodingmachine/gotenberg/internal/pkg/timeout
-#go test -race -cover -covermode=atomic github.com/thecodingmachine/gotenberg/internal/app/api
-
-# Finally testing processes shutdown.
-#go test github.com/thecodingmachine/gotenberg/internal/pkg/pm2 -run TestChromeShutdown
-#go test github.com/thecodingmachine/gotenberg/internal/pkg/pm2 -run TestUnoconvShutdown
