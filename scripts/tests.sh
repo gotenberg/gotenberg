438fd7d825f7d6bf3da78d01bc80ce9042e0f50f
Julien Neuhart <j.neuhart@thecodingmachine.com>
2019-08-27 11:20:25 +0200

    fixing CI permissions with coverage file

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,6 +6,7 @@ DOCKER_REPOSITORY="$1"
 CODE_COVERAGE="$2"
 
 touch "$PWD/coverage.txt"
+chmod 777 "$PWD/coverage.txt"
 docker build -t "$DOCKER_REPOSITORY/gotenberg:tests" -f build/tests/Dockerfile .
 
 if [ "$CODE_COVERAGE" = "1" ]; then
