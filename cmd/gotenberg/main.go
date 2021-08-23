5a439ca70ce012d43e7fa7e79993cade0fbf0556
Julien Neuhart <neuhart.julien@gmail.com>
2021-08-23 18:33:26 +0200

    fix: remove internal package and move it to cmd (#321)

--- cmd/gotenberg/main.go
+++ cmd/gotenberg/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	gotenbergapp "github.com/gotenberg/gotenberg/v7/internal/app/gotenberg"
+	gotenbergcmd "github.com/gotenberg/gotenberg/v7/cmd"
 
 	// Gotenberg modules.
 	_ "github.com/gotenberg/gotenberg/v7/pkg/standard"
-	// PDF engines.
-	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/libreoffice/pdfengine"
-	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/pdfcpu"
-	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/pdftk"
 )
 
 func main() {
-	gotenbergapp.Run()
+	gotenbergcmd.Run()
 }
--- pkg/standard/imports.go
+++ pkg/standard/imports.go
@@ -5,7 +5,10 @@ import (
 	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/chromium"
 	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/gc"
 	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/libreoffice"
+	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/libreoffice/pdfengine"
 	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/libreoffice/unoconv"
 	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/logging"
+	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/pdfcpu"
 	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/pdfengines"
+	_ "github.com/gotenberg/gotenberg/v7/pkg/modules/pdftk"
 )
