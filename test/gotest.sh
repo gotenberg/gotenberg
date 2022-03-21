0512dd1e8652ea6abed87ebfb9abe9c0418d6dc7
Julien Neuhart <neuhart.julien@gmail.com>
2022-03-21 17:13:35 +0100

    fix(tests): add -buildvcs=false as a temporary fix for go 1.18 bug (see https://github.com/golang/go/issues/51723)

--- test/gotest.sh
+++ test/gotest.sh
@@ -2,5 +2,6 @@
 
 set -x
 
-go test -race -covermode=atomic -coverprofile=/tests/coverage.txt ./...
+# TODO: remove -buildvcs=false when fix for https://github.com/golang/go/issues/51723 is live.
+go test -buildvcs=false -race -covermode=atomic -coverprofile=/tests/coverage.txt ./...
 go tool cover -html=coverage.txt -o /tests/coverage.html
