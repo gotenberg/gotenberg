61eb3ff90854fb1e06ec161755d00c7054564e60
Julien Neuhart <neuhart.julien@gmail.com>
2023-11-09 17:12:09 +0100

    chore: add a dedicated GitHub action job to test armhf build, hoping for some upstream fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,6 +10,25 @@ NOTO_COLOR_EMOJI_VERSION="$5"
 PDFTK_VERSION="$6"
 DOCKER_REPOSITORY="$7"
 
+# FIXME: for whatever reasons, platform linux/arm/v7 (armhf) is not working
+#  anymore. See https://github.com/gotenberg/gotenberg/issues/709 for more
+#  details.
+
+if [ "$GOTENBERG_VERSION" == "armhf" ]; then
+  docker buildx build \
+      --build-arg GOLANG_VERSION="$GOLANG_VERSION" \
+      --build-arg GOTENBERG_VERSION="$GOTENBERG_VERSION" \
+      --build-arg GOTENBERG_USER_GID="$GOTENBERG_USER_GID" \
+      --build-arg GOTENBERG_USER_UID="$GOTENBERG_USER_UID" \
+      --build-arg NOTO_COLOR_EMOJI_VERSION="$NOTO_COLOR_EMOJI_VERSION" \
+      --build-arg PDFTK_VERSION="$PDFTK_VERSION" \
+      --platform linux/arm/v7 \
+      -t "$DOCKER_REPOSITORY/gotenberg:armhf" \
+      -f build/Dockerfile .
+
+  exit 0
+fi
+
 if [ "$GOTENBERG_VERSION" == "edge" ]; then
   docker buildx build \
     --build-arg GOLANG_VERSION="$GOLANG_VERSION" \
@@ -20,7 +39,6 @@ if [ "$GOTENBERG_VERSION" == "edge" ]; then
     --build-arg PDFTK_VERSION="$PDFTK_VERSION" \
     --platform linux/amd64 \
     --platform linux/arm64 \
-    --platform linux/arm/v7 \
     --platform linux/386 \
     -t "$DOCKER_REPOSITORY/gotenberg:edge" \
     --push \
@@ -57,7 +75,6 @@ docker buildx build \
   --build-arg PDFTK_VERSION="$PDFTK_VERSION" \
   --platform linux/amd64 \
   --platform linux/arm64 \
-  --platform linux/arm/v7 \
   --platform linux/386 \
   -t "$DOCKER_REPOSITORY/gotenberg:latest" \
   -t "$DOCKER_REPOSITORY/gotenberg:${SEMVER[0]}" \
