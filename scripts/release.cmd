3cbf00d1d8d290e3419da93928262cbe80aa72b7
Zachary Kruse <zac@onebrief.com>
2024-01-23 15:18:49 -0500

    release.cmd update ditch buildx

--- scripts/release.cmd
+++ scripts/release.cmd
@@ -1,5 +1,5 @@
 set DOCKER_REPO_GH=ghcr.io/onebrief
-set GOTENBERG_VERSION=7.5.5
+set GOTENBERG_VERSION=8.0.1
 
 set GOLANG_VERSION=1.21
 set DOCKER_REPOSITORY=onebrief
@@ -17,7 +17,7 @@ set GOLANGCI_LINT_VERSION=v1.45.0
 @REM -t %DOCKER_REPO_GH%/gotenberg:%GOTENBERG_VERSION% ^
 @REM -t gotenberg:cookies ^
 
-docker buildx build ^
+docker build ^
   --build-arg GOLANG_VERSION=%GOLANG_VERSION% ^
   --build-arg GOTENBERG_VERSION=%GOTENBERG_VERSION% ^
   --build-arg GOTENBERG_USER_GID=%GOTENBERG_USER_GID% ^
@@ -26,6 +26,5 @@ docker buildx build ^
   --build-arg PDFTK_VERSION=%PDFTK_VERSION% ^
   --platform linux/amd64 ^
   -t %DOCKER_REPO_GH%/gotenberg:%GOTENBERG_VERSION% ^
-  -f build/Dockerfile . ^
-  --load
+  -f build/Dockerfile .
 
