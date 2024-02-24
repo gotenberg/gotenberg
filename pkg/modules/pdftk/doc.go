af9f24fc9ce82182483342c4054eb2aaaed067cd
Julien Neuhart <neuhart.julien@gmail.com>
2024-02-24 15:14:45 +0100

    chore: improve the godoc of all PDF engine packages

--- pkg/modules/pdftk/doc.go
+++ pkg/modules/pdftk/doc.go
@@ -1,8 +1,10 @@
 // Package pdftk provides an implementation of the gotenberg.PdfEngine
-// interface using the PDFtk command-line tool. This package allows for the
-// merging  of PDF files but does not support conversion to specific PDF
-// formats. The path to the PDFtk binary must be specified using the
-// PDFTK_BIN_PATH environment variable.
+// interface using the PDFtk command-line tool. This package allows for:
+//
+// 1. The merging of PDF files.
+//
+// The path to the PDFtk binary must be specified using the PDFTK_BIN_PATH
+// environment variable.
 //
 // See: https://gitlab.com/pdftk-java/pdftk.
 package pdftk
