2008ac5663515f456ea3f8c9690ff1af5d165d90
Julien Neuhart <neuhart.julien@gmail.com>
2024-02-24 15:14:45 +0100

    chore: improve the godoc of all PDF engine packages

--- pkg/modules/exiftool/doc.go
+++ pkg/modules/exiftool/doc.go
@@ -1,8 +1,11 @@
 // Package exiftool provides an implementation of the gotenberg.PdfEngine
-// interface using the ExifTool command-line tool. This package allows for reading and
-// writing of metadata, but does not support the merging of PDF files
-// nor conversion to specific PDF formats. The path to the exiftool binary must be
-// specified using the EXIFTOOL_BIN_PATH environment variable.
+// interface using the ExifTool command-line tool. This package allows for:
+//
+// 1. The reading of metadata.
+// 2. The writing of metadata.
+//
+// The path to the exiftool binary must be specified using the
+// EXIFTOOL_BIN_PATH environment variable.
 //
 // See: https://exiftool.org.
 package exiftool
