d5aa880336c5900106ea9e73022adec5c00f9dac
Julien Neuhart <neuhart.julien@gmail.com>
2025-02-07 08:41:04 +0100

    ci(build-push): fix typo in tag name

--- .github/actions/build-push/build-push.sh
+++ .github/actions/build-push/build-push.sh
@@ -68,7 +68,7 @@ if [ "${#semver[@]}" -eq 3 ]; then
   minor="${semver[1]}"
   patch="${semver[2]}"
 
-  tags+=("$DOCKER_REGISTRY/$DOCKER_REPOSITORY:latest-${arch[1]})")
+  tags+=("$DOCKER_REGISTRY/$DOCKER_REPOSITORY:latest-${arch[1]}")
   tags+=("$DOCKER_REGISTRY/$DOCKER_REPOSITORY:$major-${arch[1]}")
   tags+=("$DOCKER_REGISTRY/$DOCKER_REPOSITORY:$major.$minor-${arch[1]}r")
   tags+=("$DOCKER_REGISTRY/$DOCKER_REPOSITORY:$major.$minor.$patch-${arch[1]}")
