fbd5410480965ebeac51e733ae50ddfea803c303
Hector Zarate <Hecktorzr@gmail.com>
2025-03-26 09:57:46 +0100

    Update pkg/modules/logging/logging.go
    Co-authored-by: Julien Neuhart <neuhart.julien@gmail.com>

--- pkg/modules/logging/logging.go
+++ pkg/modules/logging/logging.go
@@ -198,16 +198,12 @@ func newLogEncoder(format string, gcpSeverity bool) (zapcore.Encoder, error) {
 
 		if gcpSeverity {
 			encCfg.EncodeLevel = gcpSeverityEncoder
+			// Those only make sense in JSON.
 			encCfg.TimeKey = "time"
 			encCfg.LevelKey = "severity"
-			encCfg.NameKey = "logger"
-			encCfg.CallerKey = "caller"
 			encCfg.MessageKey = "message"
-			encCfg.StacktraceKey = "stacktrace"
-			encCfg.LineEnding = zapcore.DefaultLineEnding
-			encCfg.EncodeTime = zapcore.RFC3339TimeEncoder
+			encCfg.EncodeTime = zapcore.ISO8601TimeEncoder
 			encCfg.EncodeDuration = zapcore.MillisDurationEncoder
-			encCfg.EncodeCaller = zapcore.ShortCallerEncoder
 		}
 	}
 
