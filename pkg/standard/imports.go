0b9bdedc7d790fbee12fd5380f93259e852ad699
Gunnar Rydberg <gunnar.rydberg@prorenata.se>
2025-10-29 13:13:29 +0100

    don't ever import chromium and pdftk
    (or else gotenberg will complain that these doesn't exists)

--- pkg/standard/imports.go
+++ pkg/standard/imports.go
@@ -3,7 +3,7 @@ package standard
 import (
 	// Standard Gotenberg modules.
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/api"
-	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/chromium"
+	//	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/chromium"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/exiftool"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/libreoffice"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/libreoffice/api"
@@ -11,7 +11,8 @@ import (
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/logging"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/pdfcpu"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/pdfengines"
-	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/pdftk"
+
+	//	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/pdftk"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/prometheus"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/qpdf"
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/webhook"
